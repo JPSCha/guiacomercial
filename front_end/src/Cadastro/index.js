f8d3f444c56bc11be62af8d250f44373eb0a2570
Joao Paulo Suzuki <123705068+JPSCha@users.noreply.github.com>
2023-05-11 16:43:01 -0300

    Update index.js

--- front_end/src/Cadastro/index.js
+++ front_end/src/Cadastro/index.js
@@ -1,3 +1,13 @@
+/**
+
+ * A função é um componente React para uma página de registro de usuário com campos de formulário para nome, email,
+ * CPF, senha e confirmação de senha. 
+ * @returns Um componente que renderiza um formulário de registro com campos de entrada para email, nome, CPF,
+ * senha e confirmação de senha, bem como um botão para enviar o formulário e um link para a página de login. O componente
+ * também inclui variáveis de estado e funções para lidar com a entrada do usuário e a submissão do formulário. 
+*/
+
+
 import { CadastroStyled, ButtonStyled, AreaHome, AreaCadastro, ImgComercio, BackHome, FormCadastro, LogoSite, TextLogin } from "./styles";
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
